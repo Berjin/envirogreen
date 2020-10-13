8512a70b5aa61ee5a3eb428e2e9ab7726eb007bd
Berjin <berjin1298@gmail.com>
2020-10-13 17:10:49 +0530

    navbar added

---
+++ src/components/Navbar/Menuitems.js
@@ -0,0 +1,32 @@
+export const MenuItems = [
+    {
+        title: 'Home',
+        url: '',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Flowers',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Plants',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Cart',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Login',
+        url: '#',
+        cName: 'nav-links-mobile'
+    },
+    {
+        title: 'Sign up',
+        url: '#',
+        cName: 'nav-links-mobile'
+    },
+]
